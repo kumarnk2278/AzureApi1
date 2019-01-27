3aa44a858c5cc8e221c5257b4c92b7f0ee4f59ec
Naresh Kumar <kumar.nk2278@gmail.com>
2019-01-27 12:16:07 +0530

    Commit AzureApi

---
+++ Respository/PostRepository.cs
@@ -0,0 +1,163 @@
+﻿using AzureApi.Models;
+using AzureApi.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+using System.Configuration;
+
+namespace AzureApi.Repository
+{
+    public class PostRepository : IPostRepository
+    {
+        BlogDBContext db;
+        public PostRepository(BlogDBContext _db)
+        {
+            db = _db;
+        }
+
+        [HttpGet]
+        public async Task<List<Category>> GetCategories()
+        {
+            if (db != null)
+            {
+                return await db.Category.ToListAsync();
+            }
+
+            return null;
+        }
+
+        [HttpGet]
+        public async Task<List<PostViewModel>> GetPosts()
+        {
+            if (db != null)
+            {
+                return await (from p in db.Post
+                              from c in db.Category
+                              where p.CategoryId == c.Id
+                              select new PostViewModel
+                              {
+                                  PostId = p.PostId,
+                                  Title = p.Title,
+                                  Description = p.Description,
+                                  CategoryId = p.CategoryId,
+                                  CategoryName = c.Name,
+                                  CreatedDate = p.CreatedDate
+                              }).ToListAsync();
+            }
+
+            return null;
+        }
+
+        [HttpGet]
+        public async Task<PostViewModel> GetPost(int? postId)
+        {
+            if (db != null)
+            {
+                return await (from p in db.Post
+                              from c in db.Category
+                              where p.PostId == postId
+                              select new PostViewModel
+                              {
+                                  PostId = p.PostId,
+                                  Title = p.Title,
+                                  Description = p.Description,
+                                  CategoryId = p.CategoryId,
+                                  CategoryName = c.Name,
+                                  CreatedDate = p.CreatedDate
+                              }).FirstOrDefaultAsync();
+            }
+
+            return null;
+        }
+
+        [HttpPost]
+        public async Task<int> AddPost(Post post)
+        {
+            if (db != null)
+            {
+              /*  // -- ** Queue ** --
+                var connString = ConfigurationManager.ConnectionStrings["MyStorageConnString"].ToString();
+                
+                //Queue is part of storge account
+                var storageAccount = CloudStorageAccount.Parse(connString);
+
+                // create Queue Client
+                var client = storageAccount.CreateCloudQueueClient();
+
+                // get Queue reference
+                var queue = client.GetQueueReference("myqueue");
+
+                // create Queue if it doesn't yet exist
+                queue.CreateIfNotExists();
+
+
+                // Create new Message
+                var message = new CloudQueueMessage("Hello Azure");
+
+                // Add / Send Message to the Queue
+                queue.AddMessage(message);
+
+
+                //// retrieve next Message from Queue
+                var message1 = queue.PeekMessage();
+
+                // get message contents
+                var contents = message1.AsString;
+
+                // -- End Queue ** --
+                */
+                await db.Post.AddAsync(post);
+                await db.SaveChangesAsync();
+
+                return post.PostId;
+            }
+
+            return 0;
+        }
+
+        [HttpDelete]
+        public async Task<int> DeletePost(int? postId)
+        {
+            int result = 0;
+
+            if (db != null)
+            {
+                //Find the post for specific post id
+                var post = await db.Post.FirstOrDefaultAsync(x => x.PostId == postId);
+
+                if (post != null)
+                {
+                    //Delete that post
+                    db.Post.Remove(post);
+
+                    //Commit the transaction
+                    result = await db.SaveChangesAsync();
+                }
+                return result;
+            }
+
+            return result;
+        }
+
+
+        [HttpPut]
+        public async Task UpdatePost(Post post)
+        {
+            if (db != null)
+            {
+                //Delete that post
+                db.Post.Update(post);
+
+                //Commit the transaction
+                await db.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AzureApi.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using AzureApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.OData.Edm;
+
+namespace AzureApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+           //services.AddOData();
+            //services.AddODataQueryFilter();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddCors();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+
+            services.AddDbContext<BlogDBContext>(item => item.UseSqlServer (Configuration.GetConnectionString("BlogDBConnection")));
+            services.AddScoped<IPostRepository, PostRepository>();
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {           
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyAPI V1");
+            });
+
+            app.UseCors(
+                options => options.WithOrigins("*").AllowAnyMethod().AllowAnyHeader()
+            );
+
+            app.UseMvc();
+
+            //b =>
+            //{
+               // b.MapODataServiceRoute("odata", "odata", GetEdmModel());
+            //});
+
+        }
+
+        /// <summary>
+        /// oData GetEdm Model
+        /// </summary>
+        /// <returns></returns>
+        private static IEdmModel GetEdmModel()
+        {
+            ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Category>("Category");
+            builder.EntitySet<Post>("Post");
+            return builder.GetEdmModel();
+        }
+    }
+}
